76b1f38a330e293b39fde637127cb36ea6dafa7d
yaning <yaning.wang@ctgtmo.com>
2018-08-30 19:02:59 +0800

    格式化一下~~~

--- vue-typescript/src/views/About.vue
+++ vue-typescript/src/views/About.vue
@@ -1,22 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    {{count}}
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        {{count}}
+    </div>
 </template>
 <script lang='ts'>
-import { Component, Vue, Watch, Provide } from "vue-property-decorator";
-import { State, Action, Getter, Mutation } from "vuex-class";
-@Component({})
-export default class Home extends Vue {
-  @State
-  count: number;
-  @Mutation
-  add: () => void;
-  @Action
-  increment: () => void;
-  mounted() {
-    this.add();
-  }
-}
+    import {
+        Component,
+        Vue,
+        Watch,
+        Provide
+    } from "vue-property-decorator";
+    import {
+        State,
+        Action,
+        Getter,
+        Mutation
+    } from "vuex-class";
+    @Component({})
+    export default class Home extends Vue {
+        @State
+        count: number;
+        @Mutation
+        add: () => void;
+        @Action
+        increment: () => void;
+        mounted() {
+            this.add();
+        }
+    }
 </script>
--- vue-typescript/src/views/Home.vue
+++ vue-typescript/src/views/Home.vue
@@ -1,86 +1,96 @@
 <template>
-    
-  <div class="home">
-      <div id="nav">
-        <router-link to="/index">Home</router-link> |
-        <router-link to="/list">About</router-link>
-      </div>
-      {{activeListItemName}}
-      {{activeList}}
-      <button @click='changeList(2222222)'>添加</button>
-      <Hello-world @on-click='clickBtn' title='asddddd' msg='阿萨德阿萨德' ref='HelloWorld'></Hello-world>
-      <router-view/>
-  </div>
+
+    <div class="home">
+        <div id="nav">
+            <router-link to="/index">Home</router-link> |
+            <router-link to="/list">About</router-link>
+        </div>
+        {{activeListItemName}}
+        {{activeList}}
+        <button @click='changeList(2222222)'>添加</button>
+        <Hello-world @on-click='clickBtn' title='asddddd' msg='阿萨德阿萨德' ref='HelloWorld'></Hello-world>
+        <router-view />
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch, Provide } from "vue-property-decorator";
-import FormValidateMixin from "@/mixins/mixin.ts";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-import axios from "axios";
+    import {
+        Component,
+        Vue,
+        Watch,
+        Provide
+    } from "vue-property-decorator";
+    import FormValidateMixin from "@/mixins/mixin.ts";
+    import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+    import axios from "axios";
 
-@Component({
-  components: {
-    HelloWorld
-  },
-  mixins: [FormValidateMixin]
-})
-export default class Home extends Vue {
-  /**
-   * @event 变量
-   */
-  @Provide()
-  activeListItemName: number = 222;
-  @Provide()
-  list: any = [];
-
-  /**
-   * @event 监控
-   */
-
-  @Watch("activeListItemName", { deep: true })
-  watchCount(newVal: number, oldVal: number) {
-    console.log("newVal", newVal, "oldVal", oldVal);
-  }
-
-  @Watch("$route.name")
-  fetchdata(to: any, from: any) {
-    console.log("newVal", to, "oldVal", from);
-  }
-  /**
-   * @event 初始化
-   */
-  mounted() {
-    console.log(this.$route.name);
-    axios({
-      method: "get",
-      url: `/api/qthl-wf-busi/website/home/getMonth/6`
+    @Component({
+        components: {
+            HelloWorld
+        },
+        mixins: [FormValidateMixin]
     })
-      .then(response => {
-        let {
-          data: { result, code }
-        } = response;
-        if (code == 0) {
-          this.list = result;
-        }
-      })
-      .catch(error => {});
-  }
-  /**
-   * @event 计算属性
-   */
+    export default class Home extends Vue {
+        /**
+         * @event 变量
+         */
+        @Provide()
+        activeListItemName: number = 222;
+        @Provide()
+        list: any = [];
 
-  get activeList(): number {
-    return this.activeListItemName + 1;
-  }
-  /**
-   * @event 事件
-   */
-  changeList(param: number) {
-    this.activeListItemName = param;
-  }
-  clickBtn(val: number) {
-    alert(val);
-  }
-}
+        /**
+         * @event 监控
+         */
+
+        @Watch("activeListItemName", {
+            deep: true
+        })
+        watchCount(newVal: number, oldVal: number) {
+            console.log("newVal", newVal, "oldVal", oldVal);
+        }
+
+        @Watch("$route.name")
+        fetchdata(to: any, from: any) {
+            console.log("newVal", to, "oldVal", from);
+        }
+        /**
+         * @event 初始化
+         */
+        mounted() {
+            console.log(this.$route.name);
+            axios({
+                    method: "get",
+                    url: `/api/qthl-wf-busi/website/home/getMonth/6`
+                })
+                .then(response => {
+                    let {
+                        data: {
+                            result,
+                            code
+                        }
+                    } = response;
+                    if (code == 0) {
+                        this.list = result;
+                    }
+                })
+                .catch(error => {});
+        }
+        /**
+         * @event 计算属性
+         */
+
+        get activeList(): number {
+            return this.activeListItemName + 1;
+        }
+        /**
+         * @event 事件
+         */
+        changeList(param: number) {
+            this.activeListItemName = param;
+        }
+        clickBtn(val: number) {
+            alert(val);
+        }
+    }
 </script>
